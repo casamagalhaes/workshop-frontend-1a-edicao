388d1610ee60b1b03ff8ffcbe577af8ff4cc8423
leotabosa <leonardoctbraga@gmail.com>
2021-07-06 09:12:29 -0300

    Adição de testes

--- worker-control-webapp/src/components/WorkersList.test.js
+++ worker-control-webapp/src/components/WorkersList.test.js
@@ -20,6 +20,24 @@ describe('src/components/WorkersList.vue', () => {
     expect(WorkersList).toBeAValidComponent()
   })
 
+  describe('Computeds', () => {
+    it('computed totalPages - SHOULD return total pages', () => {
+      const wrapper = shallowMount(WorkersList, {
+        localVue,
+        store,
+        mocks: {
+          $service: {
+            get: () => ({}),
+          },
+        },
+      })
+
+      wrapper.setData({ pagination: { pageSize: 5, totalItems: 10 } })
+
+      expect(wrapper.vm.totalPages).toEqual(2)
+    })
+  })
+
   describe('Fetching methods (load, pagination, etc)', () => {
     it('method loadWorkers - SHOULD load workers correctly and put value on data', async () => {
       // Mockando valor de retorno da request
@@ -30,6 +48,12 @@ describe('src/components/WorkersList.vue', () => {
         items: [{ id: 1 }, { id: 2 }],
         total: 2,
       }))
+
+      const store = new Vuex.Store({
+        getters: {
+          filters: () => ({ nome: 'Joãozinho' }),
+        },
+      })
 
       const wrapper = shallowMount(WorkersList, {
         localVue,
@@ -46,6 +70,37 @@ describe('src/components/WorkersList.vue', () => {
       await wrapper.vm.loadWorkers()
       expect(wrapper.vm.workers).toEqual([{ id: 1 }, { id: 2 }])
       expect(wrapper.vm.pagination.totalItems).toEqual(2)
+      expect(getMock).toHaveBeenCalled()
+
+      expect(wrapper.vm.filters).toEqual({ nome: 'Joãozinho' })
+    })
+  })
+
+  describe('Other methods', () => {
+    it('goToForm - SHOULD navigate to route named funcionarioForm', () => {
+      const pushMock = jest.fn()
+
+      const wrapper = shallowMount(WorkersList, {
+        localVue,
+        store,
+        mocks: {
+          $service: {
+            get: () => ({}),
+          },
+          $router: {
+            push: pushMock,
+          },
+        },
+      })
+
+      wrapper.vm.goToForm(1)
+
+      expect(pushMock).toHaveBeenCalled()
+      expect(pushMock).toHaveBeenCalledTimes(1)
+      expect(pushMock).toHaveBeenCalledWith({
+        name: 'funcionarioForm',
+        params: { id: 1 },
+      })
     })
   })
 })
--- worker-control-webapp/src/store/actions.test.js
+++ worker-control-webapp/src/store/actions.test.js
@@ -4,4 +4,13 @@ describe('src/store/actions', () => {
   it('actions devem ser exportadas como objeto', () => {
     expect(typeof actions === 'object').toBe(true)
   })
+
+  it('saveFilterValues', () => {
+    const commitMock = jest.fn()
+    const filters = { teste: 'teste' }
+    actions.saveFilterValues({ commit: commitMock }, filters)
+
+    expect(commitMock).toHaveBeenCalled()
+    expect(commitMock).toHaveBeenCalledWith('SET_FILTERS_VALUE', filters)
+  })
 })
